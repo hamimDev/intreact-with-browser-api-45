b6f570e43db2000eeea17da3f8563b7d46637c5c
mdhamimhowladerasif <mdhamimhowladerasif@gmail.com>
2023-11-13 03:06:29 +0600

    first commit

---
+++ js/prompt.js
@@ -0,0 +1,13 @@
+const showAlert = () =>{
+    alert('Taka da ')
+}
+
+const confirmAlert = () =>{
+    const yesNo = confirm('Taka ki debe tui');
+    console.log(yesNo);
+}
+
+const grtInfo = () =>{
+    const name = prompt('Type your name');
+    console.log(name);
+}
